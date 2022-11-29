1e9ee1b7b72d910e2a97147092fb66ef10316491
Rhys Smoker <r.smoker@gmail.com>
2022-11-29 10:48:36 -0800

    added a notices.js file to keep the notices in, having trouble importing the vars to index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
+import { MITnotice } from ".notices.js";
 
 const generateBadge = (license) => {
 	//How can I get the result of this code, badge, to appear in generateMarkdown?
@@ -30,28 +31,7 @@ const generateBadge = (license) => {
 const generateNotice = (thelicense) => {
 	let notice = "";
 	if (thelicense[0] === `MIT`) {
-		notice = `
-    Copyright (c) 2012-2022 Scott Chacon and others
-
-    Permission is hereby granted, free of charge, to any person obtaining
-    a copy of this software and associated documentation files (the
-    "Software"), to deal in the Software without restriction, including
-    without limitation the rights to use, copy, modify, merge, publish,
-    distribute, sublicense, and/or sell copies of the Software, and to
-    permit persons to whom the Software is furnished to do so, subject to
-    the following conditions:
-
-    The above copyright notice and this permission notice shall be
-    included in all copies or substantial portions of the Software.
-
-    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-    EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-    MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-    NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
-    LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
-    OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
-    WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-        `;
+		notice = MITnotice;
 	}
 	return notice;
 };
